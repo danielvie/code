98cf97562286e25644612cff3851b409f3c8c564
Daniel Vieira <danielvie@gmail.com>
2024-04-16 12:19:06 +0200

    iter

--- init.lua
+++ init.lua
@@ -569,7 +569,7 @@ require('lazy').setup({
         },
         -- gopls = {},
         pyright = {},
-        matlab_ls = {},
+        -- matlab_ls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
