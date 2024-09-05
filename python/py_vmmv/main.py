53a6eedcb0bb852f209a2342e1fb2234d83af6d1
DANIEL VIEIRA <danielvie@gmail.com>
2024-09-05 01:15:42 -0300

    Create main.py

---
+++ python/py_vmmv/main.py
@@ -0,0 +1,15 @@
+import sys
+from TaskView import TaskView
+from TaskViewModel import TaskViewModel
+from PyQt5.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    # Create ViewModel
+    task_viewmodel = TaskViewModel()
+
+    # Create and display the view
+    view = TaskView(task_viewmodel)
+
+    sys.exit(app.exec_())
