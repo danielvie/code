4e929937c2856a9f1091f4b678037126f969c8fa
Daniel Vieira <danielvie@gmail.com>
2024-09-28 13:02:19 -0300

    simple mpc

--- python/py_view/arjan/py_mvc/model.py
+++ python/py_view/arjan/py_mvc/model.py
@@ -1,21 +1,14 @@
-import sqlite3
-
 class Model:
     def __init__(self) -> None:
-        self.connection = sqlite3.connect("tasks.db")
-        self.cursor = self.connection.cursor()
-        self.cursor.execute("create table if not exists tasks (title text)")
+        self._tasks: list[str] = []
 
     def add_task(self, task: str) -> None:
-        self.cursor.execute("insert into tasks values (?)", (task,))
-        self.connection.commit()
+        self._tasks.append(task)
 
     def delete_task(self, task: str) -> None:
-        self.cursor.execute("delete from tasks where title = ?", (task,))
-        self.connection.commit()
+        for t in self._tasks:
+            if t == task:
+                self._tasks.remove(t)
 
     def get_tasks(self) -> list[str]:
-        tasks: list[str] = []
-        for row in self.cursor.execute("select title from tasks"):
-            tasks.append(row[0])
-        return tasks
+        return self._tasks
