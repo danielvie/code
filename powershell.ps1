c7aef4ad41055e898894759cbcbfee8386e80329
Daniel Vieira <danielvie@gmail.com>
2024-11-14 22:19:28 -0300

    edit aliases

---
+++ powershell.ps1
@@ -0,0 +1,11 @@
+
+Set-Alias ll eza
+Set-Alias t task
+Set-Alias m mingw32-make
+Set-Alias j just
+Set-Alias c code
+Set-Alias g git
+Set-Alias v nvim
+Set-Alias d docker
+Set-Alias gs gstatus
+Set-Alias gfe gfetch
