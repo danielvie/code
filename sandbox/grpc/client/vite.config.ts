0109fecd2765f55cb60ce82d9348c7568e63ddef
Daniel Vieira <danielvie@gmail.com>
2026-01-04 00:03:09 -0300

    iter

--- sandbox/grpc/client/vite.config.ts
+++ sandbox/grpc/client/vite.config.ts
@@ -5,9 +5,17 @@ import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    viteCommonjs({
+      include: ['src/generated/**/*.js'],
+    }),
     react(), 
-    viteCommonjs(),
   ],
+  server: {
+    port: 5174,
+  },
+  optimizeDeps: {
+    include: ['google-protobuf', 'grpc-web'],
+  },
   define: {
     // gRPC-web and google-protobuf rely on 'global'
     global: 'window',
