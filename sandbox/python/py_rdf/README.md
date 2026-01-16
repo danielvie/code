25e3ddb0e844ea2d8785ecea7680aa50a594ff2d
Daniel Vieira <danielvie@gmail.com>
2026-01-16 01:16:10 -0300

    including SPARQL test

---
+++ sandbox/python/py_rdf/README.md
@@ -0,0 +1,5 @@
+# Triples
+
+- `subject` -> the resource being described (e.g., "Bob")
+- `predicate` -> the property or relationship (e.g., "knows")
+- `object` -> the value or another resource (e.g., "Alice" or a literal like a number or string)
--- sandbox/python/py_rdf/app/main.py
+++ sandbox/python/py_rdf/app/main.py
@@ -14,9 +14,7 @@ rust_lang = TECH.rust
 cpp_lang = TECH.cpp
 
 
-def main():
-    g = Graph()
-
+def add_triples(g):
     # 3. Adding Triples: People & Roles
     g.add((bob, RDF.type, FOAF.Person))
     g.add((bob, FOAF.name, Literal("Bob Builder")))
@@ -40,30 +38,88 @@ def main():
     g.add((bob, SCHEMA.contributor, esp32_project))
     g.add((alice, SCHEMA.editor, esp32_project))
 
+    # Binding prefixes
+    g.bind("foaf", FOAF)
+    g.bind("schema", SCHEMA)
+    g.bind("mydata", MYDATA)
+    g.bind("tech", TECH)
+
+
+def main():
+    g = Graph()
+
+    add_triples(g)
+
+    update_alice_language(g)
+
     print(f"Graph initialized with {len(g)} triples.")
 
-    print("----------------")
-    print("----------------")
     print(g.serialize(format="turtle"))
-    print("----------------")
-    print("----------------")
+    update_with_sparql(g)
+    print(g.serialize(format="turtle"))
 
     print("================")
-    print(all_that_knows_cpp(g))
+    # all_that_knows_cpp(g)
+
+    query_all_alice(g)
+
+    # query_projects(g)
+
+
+def update_alice_language(g: Graph):
+    # triple to remove
+    triple = (alice, SCHEMA.knowsLanguage, rust_lang)
+
+    # remove it from graph
+    g.remove(triple)
+
+    # add a new relationship
+    g.add((alice, SCHEMA.knowsLanguage, cpp_lang))
+
+
+def query_projects(g: Graph):
+    # define the SPARQL query string
+
+    query_str = """
+    SELECT ?personName ?projectName
+    WHERE {
+        ?person foaf:name ?personName .
+        ?person schema:contributor ?project .
+        ?project schema:name ?projectName .
+    }
+    """
+
+    # execute the query
+    results = g.query(query_str)
+
+    print("\n--- Project Contributions ---")
+
+    for row in results:
+        print(f"{row.personName} worked on '{row.projectName}'")
+
+
+def query_all_alice(g: Graph):
+    for s, p, o in g.triples((alice, None, None)):
+        print("s, p, o: ", s, p, o)
+
+
+def update_with_sparql(g: Graph):
+    update_str = """
+    DELETE { ?alice schema:jobTitle "Software Architect" }
+    INSERT { ?alice schema:jobTitle "Chief Technology Officer" }
+    WHERE {
+        ?alice foaf:name "Alice Ada"
+    }
+    """
+
+    g.update(update_str)
 
 
 def all_that_knows_cpp(
     g: Graph,
 ):
-    res: list[str] = []
-    lang = cpp_lang
-
-    for person in g.subjects(SCHEMA.knowsLanguage, lang):
-        name = g.value(person, FOAF.name)
-
-        res.append(f"- {name} knows {lang}")
-
-    return res
+    for s, p, o in g.triples((alice, None, None)):
+        print(f"s, p, o: {s}, {p}, {o}")
 
 
 if __name__ == "__main__":
